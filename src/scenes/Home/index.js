d09f55062926ba72e789f8faaccc709bfc39f1ea
Renan Pallin <renanpallin@gmail.com>
2017-07-23 12:37:28 -0300

    Paginator (sem setinhas ainda)

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -5,16 +5,38 @@ import Card from './components/Card';
 export default class Home extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      page: 1,
+      itensByPage: 6
+    }
   }
 
   render() {
+    const cards = this.props.news.map(n => <Card key={ n.id } new={ n } />);
+    const { itensByPage, page } = this.state;
+    const numberOfPages = Math.ceil(this.props.news.length / itensByPage);
+
     return (
       <div>
         <h1>Home</h1>
-        {/*<Card
-          new={this.props.news[0]} />*/}
         <div className="cards">
-          { this.props.news.map(n => <Card key={ n.id } new={ n } />) }
+          { cards.splice(itensByPage * (page - 1), itensByPage * page) }
+        </div>
+        <div className="paginator">
+          <ul>
+            {
+              Array(numberOfPages)
+                .fill()
+                .map((undef, index) => (
+                    <li key={index + 1}>
+                      <span>
+                        {index + 1}
+                      </span>
+                    </li>
+                  )
+                )
+            }
+          </ul>
         </div>
       </div>
     )
