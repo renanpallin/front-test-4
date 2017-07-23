7501a3fd94cc6c0de1211fc8f195a62f2c0b490b
Renan Pallin <renanpallin@gmail.com>
2017-07-23 12:50:56 -0300

    Paginator

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -24,6 +24,7 @@ export default class Home extends React.Component {
         </div>
         <div className="paginator">
           <ul>
+            <li></li>
             {
               Array(numberOfPages)
                 .fill()
@@ -36,6 +37,7 @@ export default class Home extends React.Component {
                   )
                 )
             }
+            <li></li>
           </ul>
         </div>
       </div>
